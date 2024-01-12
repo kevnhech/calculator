964be762b31eaaebfddb6ce363cb816b4f69308b
kevin <kevnhech@gmail.com>
2024-01-12 10:18:37 -0800

    Add basic math calculating functions

---
+++ script.js
@@ -0,0 +1,20 @@
+// Step 1
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Step 2
+
